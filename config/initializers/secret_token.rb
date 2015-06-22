2851a450faf0a221e0699e62d55034eabe762291
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-06-22 13:20:51 +0200

    Basic rails 4.0 app with the rails-observers gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reproduce::Application.config.secret_key_base = '7abd78d2b78b6b595ca9a7d2e43d622a74ceb11407e01d5eb960bf3ee41381c3d3a6c0b8def21f52c84ff00cc845e6c8493e79ebfdf31867b1d395f7e919fec8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Reproduce::Application.config.session_store :cookie_store, key: '_reproduce_session'
